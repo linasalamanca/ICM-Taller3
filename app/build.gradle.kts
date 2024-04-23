c94d35859fed7a052014cc0354a275feb49787e9
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-04-22 22:48:21 -0500

    Avances 1er punto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,6 +27,11 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +42,9 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/taller3/InicioSesionActivity.kt
+++ app/src/main/java/com/example/taller3/InicioSesionActivity.kt
@@ -1,20 +1,81 @@
 package com.example.taller3
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.taller3.databinding.ActivityInicioSesionBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class InicioSesionActivity : AppCompatActivity() {
+
+    private lateinit var bindingIniSesion: ActivityInicioSesionBinding
+
+    private lateinit var autenticacion: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setContentView(R.layout.activity_inicio_sesion)
+        bindingIniSesion = ActivityInicioSesionBinding.inflate(layoutInflater)
+        autenticacion = Firebase.auth
+
+        inicioSesion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val usuarioActual = autenticacion.currentUser
+        updateUI(usuarioActual)
+    }
+
+    private fun inicioSesion(){
+        autenticacion.signInWithEmailAndPassword(bindingIniSesion.CorreoInput.text.toString(),
+            bindingIniSesion.ContrasenaInput.text.toString())
+            .addOnCompleteListener(this){ task ->
+                Log.d(TAG, "inicioCorreoSesion:onComplete:" + task.isSuccessful)
+                if(task.isSuccessful){
+                    Log.w(TAG, "inicioCorreoSesion: failure", task.exception)
+                    Toast.makeText(this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show()
+                    bindingIniSesion.CorreoInput.setText("")
+                    bindingIniSesion.ContrasenaInput.setText("")
+                }
+            }
+    }
+
+    private fun validarCampos(): Boolean{
+        var valid = true
+        val email =bindingIniSesion.CorreoInput.text.toString()
+        if(TextUtils.isEmpty(email)){
+            bindingIniSesion.CorreoInput.error = "Requerido."
+            valid = false
+        }else{
+            bindingIniSesion.CorreoInput.error = null
+        }
+
+        val contrasena = bindingIniSesion.ContrasenaInput.text.toString()
+        if(TextUtils.isEmpty(contrasena)){
+            bindingIniSesion.ContrasenaInput.error = "Requerido."
+            valid = false
+        }else{
+            bindingIniSesion.ContrasenaInput.error = null
+        }
+        return valid
+    }
+
+    private fun updateUI(usuarioActual: FirebaseUser?) {
+        if(usuarioActual != null){
+            val intentInicio = Intent(this, MapaActivity::class.java)
+            intentInicio.putExtra("usuario", usuarioActual.email)
+            startActivity(intentInicio)
+        }else{
+            bindingIniSesion.CorreoInput.setText("")
+            bindingIniSesion.ContrasenaInput.setText("")
         }
     }
 }
